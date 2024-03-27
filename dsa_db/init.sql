8adf8457c94ce0efddba4015266276119721a053
zakkii-k <59763423+zakkii-k@users.noreply.github.com>
2024-03-28 01:13:42 +0900

    バックとフロントの変更

--- dsa_db/init.sql
+++ dsa_db/init.sql
@@ -36,12 +36,13 @@ CREATE TABLE IF NOT EXISTS sub_assignments (
 
 INSERT INTO sub_assignments (id, sub_id, title, makefile, required_file_name, test_file_name, test_input, test_output, test_program) VALUES
 (1, 1, '基本課題1', 'gcd_euclid: gcd_euclid.o main_euclid.o', 'gcd_euclid.c', 'main_euclid.c', '/app/dsa_test_case/report1/in/case1.txt', '/app/dsa_test_case/report1/out/case1.txt', '/app/dsa_test_program/report1/main_iter.c'),
-(1, 2, '発展課題1', 'gcd_recursive: gcd_recursive.o main_recursive.o', 'gcd_recursive.c', 'main_recursive.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(2, 1, '基本課題1', 'linked_list: linked_list.o main_linked_list.o', 'linked_list.c', 'main_linked_list.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(2, 2, '基本課題2', 'queue: queue.o main_queue.o', 'queue.c', 'main_queue.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(2, 3, '発展課題1', 'doublylinked_list: doublylinked_list.o main_doublylinked_list.o', 'doublylinked_list.c', 'main_doublylinked_list.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(3, 1, '基本課題1', 'open_addressing: open_addressing.o main_open_addressing.o', 'open_addressing.c', 'main_open_addressing.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(4, 1, '基本課題1', "binarytree: binarytree.o main_binarytree.o", 'binarytree.c', 'main_binarytree.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(4, 2, '基本課題2', 'binarysearchtree: binarysearchtree.o main_binarysearchtree.o', 'binarysearchtree.c', 'main_binarysearchtree.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(4, 3, '発展課題1', 'binary_tree_mirror: binary_tree_mirror.o main_binary_tree_mirror.o', 'binary_tree_mirror.c', 'main_binary_tree_mirror.c', '/path/to/input', '/path/to/output', '/path/to/test/program'),
-(4, 4, '発展課題2', 'bst_advanced: bst_advanced.o main_bst_advanced.o', 'bst_advanced.c', 'main_bst_advanced.c', '/path/to/input', '/path/to/output', '/path/to/test/program');
+(1, 2, '発展課題1', 'gcd_recursive: gcd_recursive.o main_recursive.o', 'gcd_recursive.c', 'main_recursive.c', NULL, NULL, NULL),
+(2, 1, '基本課題1', 'linked_list: linked_list.o main_linked_list.o', 'linked_list.c', 'main_linked_list.c', NULL, NULL, NULL),
+(2, 2, '基本課題2', 'queue: queue.o main_queue.o', 'queue.c', 'main_queue.c', NULL, NULL, NULL),
+(2, 3, '発展課題1', 'doublylinked_list: doublylinked_list.o main_doublylinked_list.o', 'doublylinked_list.c', 'main_doublylinked_list.c', NULL, NULL, NULL),
+(3, 1, '基本課題1', 'open_addressing: open_addressing.o main_open_addressing.o', 'open_addressing.c', 'main_open_addressing.c', NULL, NULL, NULL),
+(4, 1, '基本課題1', "binarytree: binarytree.o main_binarytree.o", 'binarytree.c', 'main_binarytree.c', NULL, NULL, NULL),
+(4, 2, '基本課題2', 'binarysearchtree: binarysearchtree.o main_binarysearchtree.o', 'binarysearchtree.c', 'main_binarysearchtree.c', NULL, NULL, NULL),
+(4, 3, '発展課題1', 'binary_tree_mirror: binary_tree_mirror.o main_binary_tree_mirror.o', 'binary_tree_mirror.c', 'main_binary_tree_mirror.c', NULL, NULL, NULL),
+(4, 4, '発展課題2', 'bst_advanced: bst_advanced.o main_bst_advanced.o', 'bst_advanced.c', 'main_bst_advanced.c', NULL, NULL, NULL);
+
